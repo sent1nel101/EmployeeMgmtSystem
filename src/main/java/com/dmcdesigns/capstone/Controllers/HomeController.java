1fba6096417c1a8ab30fc4ea47776e06c7040c59
dmcda76 <dmcda76@wgu.edu>
2025-07-14 17:09:18 -0400

    rwq

--- src/main/java/com/dmcdesigns/capstone/Controllers/HomeController.java
+++ src/main/java/com/dmcdesigns/capstone/Controllers/HomeController.java
@@ -1,14 +1,13 @@
 package com.dmcdesigns.capstone.Controllers;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.ResponseEntity;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
-    public ResponseEntity<String> index() {
-        return ResponseEntity.ok("Employee Management System API - Welcome!");
+    public String index() {
+        return "forward:/index.html";
     }
 
 }
