c175a82b2227956399319b7e0894fce31ac7d219
dmcda76 <dmcda76@wgu.edu>
2025-07-14 14:38:33 -0400

    render.com edits

--- src/main/java/com/dmcdesigns/capstone/Config/DatabaseConfig.java
+++ src/main/java/com/dmcdesigns/capstone/Config/DatabaseConfig.java
@@ -1,4 +1,4 @@
-package com.dmcdesigns.capstone.config;
+package com.dmcdesigns.capstone.Config;
 
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
