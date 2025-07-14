88bd7abccf1abb6c5fbfa403a30be50eca59ac55
dmcda76 <dmcda76@wgu.edu>
2025-07-14 17:36:31 -0400

    restore frontend

--- src/main/java/com/dmcdesigns/capstone/Controllers/HomeController.java
+++ src/main/java/com/dmcdesigns/capstone/Controllers/HomeController.java
@@ -1,14 +1,14 @@
 package com.dmcdesigns.capstone.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.http.ResponseEntity;
+
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public ResponseEntity<String> index() {
-        return ResponseEntity.ok("Employee Management System API is running! Try /api/auth/login to get started.");
+    public String index() {
+        return "forward:/index.html";
     }
 
 }
